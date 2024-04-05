f590f79e1795bec74666a96d40a438977eee9266
mendozalz <mendoza124302@gmail.com>
2024-04-05 00:08:28 -0500

    agregando url para redireccionamiento mercado pago

--- app/api/apimercadopago/route.js
+++ app/api/apimercadopago/route.js
@@ -22,9 +22,9 @@ export async function POST(request) {
         }
       ],
       back_urls: {
-        success: 'https://jx4r7v2l-3000.use2.devtunnels.ms ',
-        failure: 'https://jx4r7v2l-3000.use2.devtunnels.ms ',
-        pending: 'https://jx4r7v2l-3000.use2.devtunnels.ms ',
+        success: 'https://headless-woocommerce-nu.vercel.app/gracias-por-tu-compra',
+        failure: 'https://headless-woocommerce-nu.vercel.app/error-en-tu-compra ',
+        pending: 'https://headless-woocommerce-nu.vercel.app/intentalo-mas-tarde ',
       },
       auto_return: 'approved',
       notification_url: 'https://headless-woocommerce-nu.vercel.app/api/webhooks',
