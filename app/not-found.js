a54fa6ade4b3109c95db3b17c26e94da4f19301c
Macarron <marianacifuenteszu@gmail.com>
2024-05-10 17:29:20 -0500

    pagina 404 estilizada

--- app/not-found.js
+++ app/not-found.js
@@ -1,32 +1,32 @@
 import Image from "next/image";
 import Link from "next/link";
+import { Footer } from "./components/home/Footer";
+import Whatsapp from "./components/whatsapp/Whatsapp";
+import { Header } from "./components/home/Header";
 
 export const notFound = () => {
   return (
-    <div className="my-10 px-[100px] ">
-      <Link href="/">
-        <Image
-          placeholder="empty"
-          alt="logo minca"
-          className="Image log MINCA"
-          src="/logo.png"
-          width={390 / 2}
-          height={400 / 2}
-          href="https://www.mincaelectric.com"
-        />
-      </Link>
-      <div className="grid h-screen w-full place-items-center ">
-        <div className=" text-center">
-          <h1 className="text-[64px] font-bold">Página no encontrada</h1>
-          <Link
-            className="rounded-[62px] bg-[#111] px-[30px] py-[16px] text-white"
-            href="https://www.mincaelectric.com"
-          >
-            Volver a minca electric 
-          </Link>
+    <>
+      <Header />
+      <div className="my-10 px-[100px] ">
+        <div className="grid h-screen w-full place-items-center ">
+          <div className=" text-center">
+            <h1 className="mb-10 text-[64px] font-bold">
+              Página no encontrada
+            </h1>
+            <Link
+              Link
+              href="/"
+              className="rounded-[62px] bg-[#111] px-[30px] py-[16px] text-white"
+            >
+              Volver a Minca Electric
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
+      <Footer />
+      <Whatsapp />
+    </>
   );
-}
- export default notFound
+};
+export default notFound;
