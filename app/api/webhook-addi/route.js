63ef727a8ca94680c7473eb3bc1973fdd6e9a8e8
Jehp00 <jehp01104006@gmail.com>
2024-06-13 12:05:56 -0500

    advance addi integration

--- app/api/webhook-addi/route.js
+++ app/api/webhook-addi/route.js
@@ -20,21 +20,28 @@ export async function POST(request) {
   try {
     console.log(request.headers)
     const data = await request.json();
-    console.log(data)
-    const body = {
-        orderId: data.id_order,
-        applicationId: data.applicationId,
-        approvedAmoun: data.approvedAmount,
-        currency: "COP",
-        status: data.status,
-        statusTimestamp: data.statusTimestamp,
-      }
-    if (body.status === 'APPROVED') {
-      const response = await axios.post(process.env.SERVER_ADDI_URL, body, {
+    const authorizationHeader = request.headers.get('authorization');
+    console.log(authorizationHeader)
+
+
+    if (data.status === 'APPROVED') {
+      const response = await fetch(process.env.SERVER_ADDI_URL, {
+        method: 'POST',
         headers: {
-          "Content-type": "application/json",
+            'Content-Type': 'application/json',
+            'Authorization': authorizationHeader,
         },
+        body: data,
       });
+      console.log(response)
+
+      if (response.status === 401) {
+        return NextResponse.json({ error: 'Invalid credentials, request not authorized' }, { status: 401 });
+      }
+
+      if (response.status === 500) {
+        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+      }
       // const input = data.metadata.input;
       // const cart = data.metadata.cart;
 
@@ -45,8 +52,10 @@ export async function POST(request) {
         
       await api.put(`orders/${idOrderWoocomerce}`, dataWoocommerce);
       // await sendEmail(input, cart, idOrderWoocomerce);
-      }
-    return NextResponse.json({ ok: true }, { status: 200 });
+      return NextResponse.json({ body: data }, { status: 200 });
+    } else {
+      return NextResponse.json({ error: 'Payment not approved' }, { status: 400 });
+    }
   } catch (error) {
     console.error('Error en el webhook:', error);
     return NextResponse.json({ error: 'Error' }, { status: 500 });
