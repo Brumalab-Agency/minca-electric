42f0d46d712ea883376f5dddc62d169197b8c8f5
Jehp00 <jehp01104006@gmail.com>
2024-06-26 08:52:37 -0500

    error merchant

--- app/layout.js
+++ app/layout.js
@@ -89,6 +89,29 @@ export default function RootLayout({ children }) {
               gtag('config', 'AW-802491717');
             `}
             </Script>
+          {/* <Script>
+            {`
+              !function(f,b,e,v,n,t,s) {
+                if (f.fbq) return;
+                n=f.fbq = function() { 
+                  n.callMethod ? n.callMethod.apply(n,arguments) : n.queue.push(arguments)};
+                  if (!f._fbq) f._fbq=n;
+                  n.push = n;
+                  n.loaded = !0;
+                  n.version = '2.0';
+                  n.queue = [];
+                  t = b.createElement(e);
+                  t.async = !0;
+                  t.src = v;
+                  s = b.getElementsByTagName(e)[0];
+                  s.parentNode.insertBefore(t,s)
+                }
+                (window, document,'script', 'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '618423798896056');
+                fbq('track', 'PageView');
+            `}
+          </Script>
+          <noscript><img height="1" width="1" style={{display: "none"}} src="https://www.facebook.com/tr?id=618423798896056&ev=PageView&noscript=1" /></noscript> */}
         </head>
         <body id="up" className={`${inter.className} m-auto max-w-[2560px]`}>
           <div id="scrollbar"></div>
