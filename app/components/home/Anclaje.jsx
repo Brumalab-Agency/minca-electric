072859337be9bec9bf93500fec2fc0547d9dbd2f
Macarron <marianacifuenteszu@gmail.com>
2024-07-04 15:35:01 -0500

    commented city

--- app/components/home/Anclaje.jsx
+++ app/components/home/Anclaje.jsx
@@ -12,13 +12,13 @@ export const Anclaje = ({ hero }) => {
       >
         {hero.ancla1}
       </button>
-      <button
+      {/* <button
         onClick={() => lenis.scrollTo("#city", { lerp: 0.07 })}
         className="lg:font-regular text-[15px] text-[#42454A] underline decoration-1 lg:mt-2 lg:text-base lg:leading-[25px] 2xl:text-[24px] ml-4"
       >
         Minca Motos Eléctricas
 
-      </button>
+      </button> */}
 
       {/* <button
         onClick={() => lenis.scrollTo("#ebike", { lerp: 0.07 })}
